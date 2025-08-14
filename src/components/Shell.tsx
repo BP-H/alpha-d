12312dab59d80592b36eccd8e914fea731f599cd
BP-H <blueprinthustler@gmail.com>
2025-08-14 03:31:49 -0400

    Update Shell.tsx

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,6 +1,30 @@
 // src/components/Shell.tsx
-import FeedShell from "./feed/Shell";
+import Feed from "./feed/Feed";
+import Sidebar from "./Sidebar";
+import World3D from "./World3D";
+import AssistantOrb from "./AssistantOrb";
+import ChatDock from "./ChatDock";
+import "./Shell.css";
 
 export default function Shell() {
-  return <FeedShell />;
+  return (
+    <>
+      {/* 3D/AI background layer behind everything */}
+      <div className="world-layer" aria-hidden>
+        <World3D />
+      </div>
+
+      {/* Sleek pop-out sidebar (top-left) */}
+      <Sidebar />
+
+      {/* Glassy feed viewport */}
+      <main className="feed-viewport">
+        <Feed />
+      </main>
+
+      {/* Bottom utilities */}
+      <ChatDock />
+      <AssistantOrb />
+    </>
+  );
 }
