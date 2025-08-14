e66a501f4db307f3605984bbd49bc2e58d302d89
BP-H <blueprinthustler@gmail.com>
2025-08-14 03:01:41 -0400

    Update Shell.tsx

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,31 +1,6 @@
 // src/components/Shell.tsx
-import AssistantOrb from "./AssistantOrb";
-import World3D from "./World3D";
-import Feed from "./feed/Feed";
-import ChatDock from "./ChatDock";
-import BrandBadge from "./BrandBadge";
+import FeedShell from "./feed/Shell";
 
 export default function Shell() {
-  return (
-    <>
-      {/* 3D / AI background behind everything */}
-      <div className="world-layer" style={{ position: "fixed", inset: 0, zIndex: 0, pointerEvents: "none" }}>
-        <World3D selected={null} onBack={() => {}} />
-      </div>
-
-      {/* Foreground feed */}
-      <main className="content-viewport" style={{ height: "100dvh", overflow: "auto", position: "relative", zIndex: 10 }}>
-        <div className="feed-wrap" style={{ padding: "8px 0 80px" }}>
-          <Feed />
-        </div>
-      </main>
-
-      {/* Top-left brand button (required prop) */}
-      <BrandBadge onEnterUniverse={() => { /* no-op for now */ }} />
-
-      {/* Chat + Orb */}
-      <ChatDock />
-      <AssistantOrb />
-    </>
-  );
+  return <FeedShell />;
 }
