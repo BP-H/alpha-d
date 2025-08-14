58df75a654dc488204a38455165ed35e37600723
BP-H <blueprinthustler@gmail.com>
2025-08-14 04:20:30 -0400

    Update Index.ts

--- src/components/feed/Index.ts
+++ src/components/feed/Index.ts
@@ -1,4 +1,4 @@
-// Correct, safe re-exports. Default is Feed for: import Feed from ".../feed"
+// src/components/feed/index.ts
 export { default } from "./Feed";
 export { default as Feed } from "./Feed";
 export { default as PostCard } from "./PostCard";
