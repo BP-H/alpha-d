26b362ccf226963a4aac2becd46a6510997c6101
BP-H <blueprinthustler@gmail.com>
2025-08-14 02:02:08 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+// src/App.tsx
 import React from "react";
 import Shell from "./components/Shell";
+
+// make sure your global styles are loaded
+import "./index.css";
+import "./styles.css";
 
 export default function App() {
   return <Shell />;
